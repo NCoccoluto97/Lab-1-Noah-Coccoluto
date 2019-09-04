46060ad8333a1429d8ef43dfa6a35be26d2eb3de
NCoccoluto97 <naoh.coccoluto@uvm.edu>
2019-09-04 15:41:14 -0400

    Test

---
+++ sitemap.php
@@ -0,0 +1,198 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+    <head>
+        <title>---> CHECK THIS TAB OUT <---</title>
+        <meta charset="utf-8">
+        <meta name="author" content="Noah Coccoluto">
+        <meta name="description" content="A site map to all my assignments for the cs008 course at UVM.">
+        <style type="text/css">
+            figure {
+                border: thick blue solid darkslategray;
+                border-radius: 0.6em;
+                padding: 0.8em;
+                text-align: center;
+            }
+
+            figcaption {
+                color: #839e99;
+                font-style: italic;
+                text-align: center;
+            }
+
+            img{
+                border-radius: 0.6em;
+                max-width: 100%
+            }
+
+            .right{
+                float: right;
+                margin-left: 1em;
+            }
+
+            .small {
+                width: 150px;
+            }
+        </style>
+    </head>
+
+    <body>
+        <figure class="right small">
+            <img alt="" src="WIN_20170830_12_22_01_Pro.jpg">
+            <figcaption>Me in my natural element.</figcaption>
+        </figure>
+        <h1>Noah Coccoluto</h1>
+
+        <ol>
+            <li><a href="tryme.php">Link to my 'tryme.php' website</a></li>
+            <li>Click Below for Lab I:
+                <ol>
+                    <li><a href="lab1.php">For Lab I CLICK HERE (√)</a></li>
+                </ol>
+            </li>
+            <li>Click Below for Lab II:
+                <ol>
+                    <li><a href="lab2.php">For Lab II CLICK HERE (√)</a></li>
+                    <li><a href="css/custom.css">For 'custom.css' CLICK HERE (√)</a></li>
+                </ol>
+            </li>
+            <li>Click Below for Lab III:
+                <ol>
+                    <li><a href="lab3.php">For Lab III CLICK HERE(√)</a></li>
+                    <li><a href="css/custom.css">For 'custom.css' CLICK HERE (√)</a></li>
+                </ol>
+            </li>
+            <li>Click Below for Lab IV:
+                <ol>
+                    <li><a href="lab4.php">For Lab IV CLICK HERE(√)</a></li>
+                    <li><a href="css/custom.css">For 'custom.css' CLICK HERE (√)</a></li>
+                </ol>
+            </li>
+            <li>Click Below for Lab V:
+                <ol>
+                    <li><a href="lab5.php">For Lab V CLICK HERE(√)</a></li>
+                    <li><a href="css/custom.css">For 'custom.css' CLICK HERE (√)</a></li>
+                </ol>
+            </li>
+            <li>Click Below for Lab VI:
+                <ol>
+                    <li><a href="lab6/">For Lab VI CLICK HERE(√)</a>
+                        <ol>
+                            <li><a href="lab6/art.php">Art</a></li>
+                            <li><a href="lab6/contest.php">Contest</a></li>
+                            <li><a href="lab6/index.php">Index</a></li>
+                            <li><a href="lab6/news.php">News</a></li>
+                            <li><a href="lab6/weather.php">Weather</a></li>
+                        </ol>
+                    </li>
+                    <li><a href="css/custom.css">For 'custom.css' CLICK HERE (√)</a></li>
+                </ol>
+            </li>
+            <li>Click Below for Lab VIII:
+                <ol>
+                    <li><a href="lab8/">For Lab VIII CLICK HERE(√)</a>
+                        <ol>
+                            <li><a href="lab8/art.php">Art</a></li>
+                            <li><a href="lab8/contest.php">Contest</a></li>
+                            <li><a href="lab8/index.php">Home</a></li>
+                            <li><a href="lab8/form.php">Join</a></li>
+                            <li><a href="lab8/news.php">News</a></li>
+                            <li><a href="lab8/weather.php">Weather</a></li>
+                            <li>Project Files:
+                                <ol>
+                                    <li><a href="lab8/join1.0.pdf">Specification Document</a></li>
+                                    <li><a href="css/custom.css">For 'custom.css' CLICK HERE (√)</a></li>
+                                </ol>
+                            </li>
+                        </ol>
+                    </li>
+                </ol>
+            </li>
+            <li>Click Below for Lab IX:
+                <ol>
+                    <li><a href="lab9/">For Lab IX CLICK HERE(√)</a>
+                        <ol>
+                            <li><a href="lab9/art.php">Art</a></li>
+                            <li><a href="lab9/contest.php">Contest</a></li>
+                            <li><a href="lab9/index.php">Home</a></li>
+                            <li><a href="lab9/form.php">Join</a></li>
+                            <li><a href="lab9/news.php">News</a></li>
+                            <li><a href="lab9/weather.php">Weather</a></li>
+                            <li>Project Files:
+                                <ol>
+                                    <li><a href="lab9/join1.1.pdf">Specification Document</a></li>
+                                    <li><a href="css/custom.css">For 'custom.css' CLICK HERE (√)</a></li>
+                                </ol>
+                            </li>
+                        </ol>
+                    </li>
+                </ol>
+            </li>
+            <li>Click Below for Lab X:
+                <ol>
+                    <li><a href="lab10/">For Lab X CLICK HERE(√)</a>
+                        <ol>
+                            <li><a href="lab10/art.php">Art</a></li>
+                            <li><a href="lab10/contest.php">Contest</a></li>
+                            <li><a href="lab10/index.php">Home</a></li>
+                            <li><a href="lab10/form.php">Join</a></li>
+                            <li><a href="lab10/news.php">News</a></li>
+                            <li><a href="lab10/weather.php">Weather</a></li>
+                            <li>Project Files:
+                                <ol>
+                                    <li><a href="lab10/join1.2.pdf">Specification Document</a></li>
+                                    <li><a href="css/custom.css">For 'custom.css' CLICK HERE (√)</a></li>
+                                </ol>
+                            </li>
+                        </ol>
+                    </li>
+                </ol>
+            </li>
+            <li>Click Below for Lab XI:
+                <ol>
+                    <li><a href="lab11/">For Lab XI CLICK HERE(√)</a>
+                        <ol>
+                            <li><a href="lab11/art.php">Art</a></li>
+                            <li><a href="lab11/contest.php">Contest</a></li>
+                            <li><a href="lab11/index.php">Home</a></li>
+                            <li><a href="lab11/form.php">Join</a></li>
+                            <li><a href="lab11/news.php">News</a></li>
+                            <li><a href="lab11/weather.php">Weather</a></li>
+                            <li>Project Files:
+                                <ol>
+                                    <li><a href="registration.csv">Download 'registration.csv' HERE</a>
+                                    <li><a href="lab11/specs.pdf">Specification Document</a></li>
+                                    <li><a href="css/custom.css">For 'custom.css' CLICK HERE (√)</a></li>
+                                </ol>
+                            </li>
+                        </ol>
+                    </li>
+                </ol>
+            </li>
+            <li>Click Below for the Final Lab:
+                <ol>
+                    <li><a href="final/index.php">For the final lab CLICK HERE(√)</a>
+                        <ol>
+                            <li><a href="final/index.php">Home</a></li>
+                            <li><a href="final/nrelease.php">New Releases</a></li>
+                            <li><a href="final/shirts.php">Shopping</a></li>
+                            <li><a href="final/about.php">About</a></li>
+                            <li><a href="final/form.php">Join</a></li>
+                            <li><a href="final/contact.php">Contact</a></li>
+                            <li>Project Files:
+                                <ol>
+                                    <li><a href="data/registration.csv">Download 'registration.csv' HERE</a>
+                                    <li><a href="final/specs.pdf">Specification Document</a></li>
+                                    <li><a href="css/custom.css">For 'custom.css' CLICK HERE (√)</a></li>
+                                </ol>
+                            </li>
+                        </ol>
+                    </li>
+                </ol>
+            </li>
+            <!-- add more li (list) elements as needed for each lab -->
+
+        </ol>
+
+    </body>
+</html>
